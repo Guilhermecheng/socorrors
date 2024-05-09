0de5741ea25f0a9f6ba70895f9b21e0c0a002c09
Guilhermecheng <guilherme.cheng@gmail.com>
2024-05-09 12:34:25 -0300

    it works

--- sections/ItWorks.tsx
+++ sections/ItWorks.tsx
@@ -7,53 +7,33 @@ export interface Props {
    * @default It Works!
    */
   name?: string;
-  count?: number;
+  subtext?: string;
 }
 
-export default function Section({ name = "It Works!", count = 0 }: Props) {
-  /**
-   * usePartialSection is a nice hook for getting the HTMX link to render this section,
-   * but with the following Props
-   */
-  const downLink = usePartialSection({ props: { count: count - 1 } });
-  const upLink = usePartialSection({ props: { count: count + 1 } });
+export default function Section({ 
+  name = "Hello World!", 
+  subtext = "It's a new Deco site!", 
+}: Props) {
 
   return (
     <div
       id="it-works"
-      class="container py-10 flex flex-col h-screen w-full items-center justify-center gap-16"
+      class="container py-10 flex flex-col h-screen w-full items-center justify-center gap-16 bg-zinc-800"
     >
-      <div class="leading-10 text-6xl" dangerouslySetInnerHTML={{
-        __html: name,
-      }}/>
+      <div class="grid grid-cols-1 lg:grid-cols-2 w-full h-full text-zinc-100">
+        <div>
+          <div class="w-full h-full flex items-center justify-center text-4xl font-semibold" dangerouslySetInnerHTML={{
+            __html: name,
+          }} />
 
-      <div class="flex flex-col items-center justify-center gap-2">
-        <div class="flex items-center gap-4">
-          <button
-            hx-target="#it-works"
-            hx-swap="outerHTML"
-            hx-get={downLink["f-partial"]} // htmx link for this section with the down vote props
-            class="btn btn-sm btn-circle btn-outline no-animation"
-          >
-            <span class="inline [.htmx-request_&]:hidden">
-              -
-            </span>
-            <span class="loading loading-spinner hidden [.htmx-request_&]:inline" />
-          </button>
-          <span>{count}</span>
-          <button
-            hx-target="#it-works"
-            hx-swap="outerHTML"
-            hx-get={upLink["f-partial"]} // htmx link for this section with the up vote props
-            class="btn btn-sm btn-circle btn-outline no-animation"
-          >
-            <span class="inline [.htmx-request_&]:hidden">
-              +
-            </span>
-            <span class="loading loading-spinner hidden [.htmx-request_&]:inline" />
-          </button>
+          <div class="w-full h-full flex items-center justify-center text-2xl" dangerouslySetInnerHTML={{
+            __html: subtext,
+          }} />
         </div>
-        <div class="text-sm">Powered by HTMX</div>
+        
+        <div class="w-full h-full flex items-center justify-center text-4xl font-semibold" >
+          pix
+        </div>
       </div>
     </div>
   );
